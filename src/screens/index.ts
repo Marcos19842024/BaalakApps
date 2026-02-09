829b7471b75e0f4f67fe48be422accf838537ffb
Marcos19842024 <chable240608@gmail.com>
2026-02-09 08:00:14 -0600

    9.2

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,7 +2,6 @@ export { ChecklistScreen } from './ChecklistScreen';
 export { TemplateChecklistScreen } from './TemplateChecklistScreen';
 export { MainMenuScreen } from './MainMenuScreen';
 export { RemindersScreen } from './RemindersScreen';
-export { UpdatesScreen } from './UpdatesScreen';
 export { MessageBubble } from './MessageBubble';
 export { ReportsScreen } from './ReportScreen';
 export { DriveFilesScreen } from './DriveFilesScreen';
